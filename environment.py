80c3516d77c7b2d739c7d772794afd1c57b904ff
Wirachapong.suw <73430548+wirachapong@users.noreply.github.com>
2023-10-26 11:04:33 -0400

    Update environment.py

--- environment.py
+++ environment.py
@@ -10,15 +10,27 @@ class Environment:
     def __init__(self):
         self.NNOfPerson= NNOfPerson
         self.persons = [Person(self.NNOfPerson,np.random.choice(EDUCATION_LEVELS)) for _ in range(NUM_PERSONS)]
-        self.PolicyPlannerAgent= PolicyPlannerAgent(2*len(self.persons),len(ACTIONS))
+        self.PolicyPlannerAgent= PolicyPlannerAgent(2*len(self.persons)+7,len(ACTIONS))
+        # len 2*len(self.persons)+7 = from net_worths+educations+tax_rate
+
+    # class PolicyPlannerAgent:
+    #     def __init__(self, input_dim, num_actions):
+    #         self.model = QNetwork(input_dim, num_actions)
+    #         self.current_tax_rate = [10,12,22,24,32,35,37]
+    #         self.memory = []  # For experience replay
+    #         self.history_of_auctions = []
+    #         self.optimizer = optim.Adam(self.model.parameters(), lr=ALPHA)
 
     def get_state(self):
         # This method compiles the net worths and education levels of all persons
         # into a single list representing the current state.
         net_worths = [person.net_worth for person in self.persons]
         educations = [person.education_level for person in self.persons]
-        
-        state = net_worths + educations
+        tax_rate= PolicyPlannerAgent.current_tax_rate
+
+        # it should also be history_of_auctions here but I'm not sure how to include it.
+
+        state = net_worths + educations+tax_rate
         return state
 
     def step(self, action):
