840d99baff7aeba7ed4eda2bb6c318e6f159038f
RasmusTorp <s214597@dtu.dk>
2023-10-29 21:47:41 -0400

    correcting to correct types

--- environment_1nn.py
+++ environment_1nn.py
@@ -9,8 +9,8 @@ class Environment_1nn(Environment):
     def __init__(self, n_persons:int):
         
         education_level_turn0 = 1.0
-        net_worth_turn0 = 0
-        base_salary = 400
+        net_worth_turn0 = 0.0
+        base_salary = 400.0
         n_brackets = 7
         self.person_model = NNOfPerson(2, 2) # QNetwork[net_worth, potential_income] -> [earn, learn]
 
