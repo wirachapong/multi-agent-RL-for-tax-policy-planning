df65a28212f6351ad691863a57f790bf707f7f4c
RasmusTorp <s214597@dtu.dk>
2023-10-29 21:15:49 -0400

    fixed bugs and updated main to work with the rest

--- main.py
+++ main.py
@@ -2,46 +2,57 @@
 from environment import Environment
 from policyplanneragent import PolicyPlannerAgent
 from person import Person
+from constants import NUM_PERSONS
 
 def main():
-    env = Environment()
+    
+    env = Environment(NUM_PERSONS)
     EPSILON = 0.1  # Consider moving constants to a separate config file or module
     total_reward_policy_planner = 0
     total_reward_individual = 0
-    num_episodes = 5000  # You might need more episodes for training
+    num_episodes = 50  # You might need more episodes for training
 
     for episode in range(num_episodes):
         print('Episode', episode)
         total_reward_policy_planner += simulate_episode(env)[0]
+        
         total_reward_individual += simulate_episode(env)[1]
         # Optionally decrease epsilon over time to reduce exploration
         if EPSILON > 0.01:
-            EPSILON *= 0.995
+            EPSILON *= 0.995   
 
     print(f"Total reward after {num_episodes} episodes: {[total_reward_policy_planner,total_reward_individual]}")
 
 def simulate_episode(env):
     current_state = env.get_state()
     
+    #! Either this in main.py or in Environment.py
+    # #  Here should be the space that each individual start doing actions
+    # for person in env.persons:
+    #     current_state = person.get_state()
+    #     person_action = person.select_action()
 
+    #     person.take_action(person_action, env.policy_planner_agent.tax_rate_for_income)
+        
+    #     person_reward = person.get_reward()
+    #     person_next_state = person.get_state()
 
-    #  Here should be the space that each individual start doing actions
-    for i in env.persons:
-        person_action = i.select_action(current_state)
-        i.earn_revenue()
-        person_next_state = env.step(person_action) 
-        person_reward = i.get_reward(0, env.persons) 
-        i.remember(current_state, person_action, person_reward, person_next_state)
+    #     person.remember(current_state, person_action, person_reward, person_next_state)
+        
+    #     #! Maybe not do this to batch training later instead
+    #     person.replay()
 
-    #
     action = env.PolicyPlannerAgent.select_action(current_state)
     total_cost = env.PolicyPlannerAgent.apply_action(action, env.persons)  # Assumes you've added this method to DQNAgent, similar to PolicyMaker
-    next_state = env.step(action)  # Adjusted for simplicity; step might need more info
+    next_state = env.persons_step()
     reward_policy_planner = env.PolicyPlannerAgent.get_reward(0, env.persons)  # Assumes you've added this method to DQNAgent, similar to PolicyMaker
+    
     # we used 0 for now in the (a,b) for previously used get_reward function due to how there's a change in how the policy changed from our first structure
     env.PolicyPlannerAgent.remember(current_state, action, reward_policy_planner, next_state)
     env.PolicyPlannerAgent.replay()  # Experience replay
-    return [reward_policy_planner]
+
+    total_reward_individual = sum([person.get_reward() for person in env.persons])
+    return [reward_policy_planner, total_reward_individual]
 
 if __name__ == "__main__":
     main()
